a446e2b82a0ce7d93919a12b4381d470ebea33e5
hydrocat <vdrbandeiras@gmail.com>
2018-09-12 11:34:21 +0100

    add yabi-backend submodule

--- yabi-backend/src/main/java/ipb/yabi/Directory/DirectoryRepository.java
+++
@@ -1,8 +0,0 @@
-package ipb.yabi.Directory;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface DirectoryRepository extends PagingAndSortingRepository<Directory, Long> {
-
-    public Directory findByName(String name);
-}
--- yabi-backend/src/main/java/ipb/yabi/SqlQuery/SqlQueryRepository.java
+++
@@ -1,8 +0,0 @@
-package ipb.yabi.SqlQuery;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface SqlQueryRepository extends PagingAndSortingRepository<SqlQuery, Long> {
-    public SqlQuery findByName(String name);
-
-}
